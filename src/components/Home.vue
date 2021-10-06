11845f1311c7e202c7cd65a7b5e9c9ff77628f6c
闫威 <779463900@qq.com>
2021-10-06 17:21:12 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login') // 重定向到登录页
+    },
+  },
+}
+</script>
+
+<style>
+</style>
