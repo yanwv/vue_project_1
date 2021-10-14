d7a79e8a2f951228feb7a98da15feec37c406902
闫威 <779463900@qq.com>
2021-10-14 20:33:10 +0800

    完成权限功能

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -74,6 +74,7 @@
                 type="warning"
                 icon="el-icon-setting"
                 size="mini"
+                @click="setRole(scope.row)"
               ></el-button>
             </el-tooltip>
           </template>
@@ -153,6 +154,34 @@
       <span slot="footer" class="dialog-footer">
         <el-button @click="editDialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="edituUserInfo">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 分配角色窗口 -->
+    <el-dialog
+      title="分配角色"
+      :visible.sync="setRoleDialogVisible"
+      width="50%"
+      @close="setRoleDialogClosed"
+    >
+      <div>
+        <p>当前用户：{{ userInfo.username }}</p>
+        <p>当前角色：{{ userInfo.role_name }}</p>
+        <p>
+          分配新角色：
+          <el-select v-model="selectRoleId" placeholder="请选择">
+            <el-option
+              v-for="item in roleList"
+              :key="item.id"
+              :label="item.roleName"
+              :value="item.id"
+            >
+            </el-option>
+          </el-select>
+        </p>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="setRoleDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveRoleInfo"> 确 定 </el-button>
       </span>
     </el-dialog>
   </div>
@@ -272,6 +301,14 @@ export default {
           },
         ],
       },
+      // 控制分配角色窗口的显示与隐藏
+      setRoleDialogVisible: false,
+      // 被分配角色的用户信息
+      userInfo: {},
+      // 所有角色数据列表
+      roleList: [],
+      // 已选中的角色id值
+      selectRoleId: '',
     }
   },
   created() {
@@ -386,6 +423,40 @@ export default {
       }
       this.$message.success('删除用户成功')
       this.getUserList()
+    },
+    // 展示分配角色的窗口
+    async setRole(userInfo) {
+      this.userInfo = userInfo
+      // 在展示窗口前获取所有角色的列表
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取角色列表失败')
+      }
+      this.roleList = res.data
+      this.setRoleDialogVisible = true
+    },
+    // 点击按钮分配角色
+    async saveRoleInfo() {
+      if (!this.selectRoleId) {
+        return this.$message.error('请选择要分配的角色')
+      }
+      const { data: res } = await this.$http.put(
+        `users/${this.userInfo.id}/role`,
+        {
+          rid: this.selectRoleId,
+        }
+      )
+      if (res.meta.status !== 200) {
+        return this.$message.error('更新角色失败')
+      }
+      this.$message.success('更新角色成功')
+      this.getUserList()
+      this.setRoleDialogVisible = false
+    },
+    // 监听分配角色窗口的关闭事件
+    setRoleDialogClosed() {
+      this.selectRoleId = ''
+      this.userInfo = {}
     },
   },
 }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,7 +3,8 @@ import {
   Button, Form, FormItem, Input, Message, Container,
   Header, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb,
   BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch,
-  Tooltip, Pagination, Dialog, MessageBox
+  Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select,
+  Option
 } from 'element-ui'
 
 Vue.use(Button)
@@ -28,6 +29,10 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Option)
+Vue.use(Select)
 
 // 全局挂载
 Vue.prototype.$message = Message
